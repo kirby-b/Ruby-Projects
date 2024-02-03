f4a3e05e613ea30609f523bf3ba8cfa59faa6a58
kirby-b <kirby.brodie66@gmail.com>
2024-02-03 09:41:42 -0500

    Update BogoSort.rb
    -Works now
    -Makes your computer sound like a jet engine, as a bogo sort does

--- Bogo/BogoSort.rb
+++ Bogo/BogoSort.rb
@@ -1,16 +1,26 @@
-class BogoSort:
-  numbers = [1,3,5,4,9,6,8,7,2,0]
-  while (isSorted(numbers) == false)
-      for x in 0...numbers.length()
-          puts numbers[x]
-      numbers = numbers.shuffle()
-  #Randomizes the list item positions until the list is sorted
-  print("Sorted!")
+class BogoSort
+    def sort()
+        numbers = [1,3,5,4,9,6,8,7,2,0]
+        while (isSorted(numbers) == false)
+            for x in 0...numbers.length()
+                print numbers[x]
+                numbers = numbers.shuffle()
+            end
+        end
+        #Randomizes the list item positions until the list is sorted
+        print("Sorted!")
+    end
+    def isSorted(nums)
+        l = nums.length()
+        for x in 0...(l-1)
+            if nums[x] > nums[x + 1]
+                return false
+            end
+        end
+        return true
+        #Checks if it is sorted
+    end
+end
+b = BogoSort.new
 
-  def isSorted(nums):
-      l = nums.length()
-      for x in 0...(l-1)
-          if nums[x] > nums[x + 1]
-              return false
-      return true
-      #Checks if it is sorted
+b.sort()
