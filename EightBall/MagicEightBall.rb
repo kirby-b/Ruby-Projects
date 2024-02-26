0c47a4c54248e911f96503d3119075f1178c49fc
kirby-b <kirby.brodie66@gmail.com>
2024-02-25 20:15:37 -0500

    Update MagicEightBall.rb

--- EightBall/MagicEightBall.rb
+++ EightBall/MagicEightBall.rb
@@ -1,12 +1,12 @@
 class MagicEightBall
-  def shakeBall
+  def shake_ball
     answers = ["It is certain", "Reply hazy, try again", "As I see it, yes", "Don't count on it", "Ask someone else",
       "It is decidedly so", "Ask again later", "Most likely", "My reply is no", "Wubba Lubba Dub Dub", 
       "Without a doubt", "Better not tell you now", "Outlook good", "My sources say no", "Outlook awful",
       "Yes definitely", "Cannot predict now", "Yes", "Outlook not so good", "Please wipe the screen, I couldnt hear you",
       "You may rely on it", "Concentrate and ask again", "Signs point to yes", "Very doubtful", "Perhaps therapy"]
-    getAns = answers[1 + rand(answers.length())]
-    puts getAns  #Gets the eightball answer
+    get_ans = answers[1 + rand(answers.length)]
+    puts get_ans  #Gets the eight ball answer
   end
 end
 
@@ -14,10 +14,10 @@ end
 
 puts "What is your question?"
 question = gets.chomp
-if !question
+if question.empty?
   puts "You have to ask a question!"
 else
   puts "Shaking the magic 8 ball!"
-  ball = EightBall.new
-  ball.shakeBall()
+  ball = MagicEightBall.new
+  ball.shake_ball
 end
