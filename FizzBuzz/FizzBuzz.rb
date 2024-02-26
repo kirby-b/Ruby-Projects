fe6ba62a216c515abf46af17a8f84818e921481f
kirby-b <kirby.brodie66@gmail.com>
2024-02-25 20:15:40 -0500

    Update FizzBuzz.rb

--- FizzBuzz/FizzBuzz.rb
+++ FizzBuzz/FizzBuzz.rb
@@ -1,9 +1,9 @@
 class FizzBuzz
-    def initialize(num)
-        @n = num
+    def initialize(number)
+        @num = number + 1
     end
-    def toFizzToBuzz
-        for i in 0...@n do
+    def to_fizz_to_buzz
+        (0...@num).each { |i|
             if i % 5 == 0 && i % 3 == 0 #If it is divisible by 5 and 3 it prints FizzBuzz
                 puts "FizzBuzz"
             elsif i % 3 != 0 && i % 5 != 0 #If it is divisible by none it prints the number
@@ -12,9 +12,9 @@ class FizzBuzz
                 puts "Fizz"
             elsif i % 5 == 0 #If it is divisible by 5 it prints Buzz
                 puts "Buzz"
-            #Prints a phrase based on the current number being tested
-            end 
-        end
+                #Prints a phrase based on the current number being tested
+            end
+        }
     end
 end
 fizz1 = FizzBuzz.new(3)
@@ -22,10 +22,10 @@ fizz2 = FizzBuzz.new(5)
 fizz3 = FizzBuzz.new(10)
 fizz4 = FizzBuzz.new(20)
 puts "\nInput Number 3:"
-fizz1.toFizzToBuzz
+fizz1.to_fizz_to_buzz
 puts "\nInput Number 5:"
-fizz2.toFizzToBuzz
+fizz2.to_fizz_to_buzz
 puts "\nInput Number 10:"
-fizz3.toFizzToBuzz
+fizz3.to_fizz_to_buzz
 puts "\nInput Number 20:"
-fizz4.toFizzToBuzz
+fizz4.to_fizz_to_buzz
