5ccc922566eaddcff10578a125df6b0268d96072
kirby-b <kirby.brodie66@gmail.com>
2024-02-25 20:15:33 -0500

    Update BogoSort.rb

--- Bogo/BogoSort.rb
+++ Bogo/BogoSort.rb
@@ -1,26 +1,26 @@
 class BogoSort
-    def sort()
+    def sort
         numbers = [1,3,5,4,9,6,8,7,2,0]
-        while (isSorted(numbers) == false)
-            for x in 0...numbers.length()
+        until is_sorted(numbers)
+            (0...numbers.length).each { |x|
                 print numbers[x]
-                numbers = numbers.shuffle()
-            end
+                numbers = numbers.shuffle
+            }
         end
         #Randomizes the list item positions until the list is sorted
         print("Sorted!")
     end
-    def isSorted(nums)
-        l = nums.length()
-        for x in 0...(l-1)
+    def is_sorted(nums)
+        l = nums.length
+        (0...(l - 1)).each { |x|
             if nums[x] > nums[x + 1]
                 return false
             end
-        end
-        return true
+        }
+        true
         #Checks if it is sorted
     end
 end
 b = BogoSort.new
 
-b.sort()
+b.sort
