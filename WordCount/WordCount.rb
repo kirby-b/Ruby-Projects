6880ac4420ef18701be6f1a5bf3c19ccfd17ddb3
kirby-b <kirby.brodie66@gmail.com>
2024-02-25 20:15:45 -0500

    Update WordCount.rb

--- WordCount/WordCount.rb
+++ WordCount/WordCount.rb
@@ -1,27 +1,27 @@
 class WordCount
   def initialize(words)
-        @wordString = words
+        @word_string = words
   end
-  def wordCount
+  def word_count
     #Uses array/list to count words
     #Makes a list by splitting the words with spaces as delimiters
     y = 0
     x = 0
-    countable = @wordString.split(" ")
-    while x < countable.length()
+    countable = @word_string.split(" ")
+    while x < countable.length
         if countable[x] == "" || countable[x].strip.empty?
             countable.pop(x)#Pops the index if it is empty or a space
         else
             x +=1
         end
     end
-    counted = countable.length()
+    counted = countable.length
     puts "This sentence contains #{counted} words" 
         
   end
 end
 
 puts "Please input a sentence:\n"
-wordString = gets.chomp
-count = WordCount.new(wordString)
-count.wordCount
+word_string = gets.chomp
+count = WordCount.new(word_string)
+count.word_count
