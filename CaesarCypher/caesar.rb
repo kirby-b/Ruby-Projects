2d0242e4836c8bef0c480bd78b54ad7298cf3be9
Brodie E Kirby <118732723+kirby-b@users.noreply.github.com>
2024-05-12 08:54:03 -0400

    Update caesar.rb

--- CaesarCypher/caesar.rb
+++ CaesarCypher/caesar.rb
@@ -1,4 +1,4 @@
-class Cypher:
+class Cypher
     symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()?,.<>|~`ÁÉÍÓÚÝáéíóúý"
     max_key_size = symbols.length
     # Holds the number of characters in the symbols variable (maximum shift number)
@@ -6,7 +6,15 @@ class Cypher:
     def main()
         mode = get_mode()
         message = get_message()
-
+        if mode[0] != "b"
+            key = get_key()
+            puts "Your translated message is"
+            puts get_translated_message(mode, message, key)
+        else
+            for key in range(1..max_key_size + 1)
+                puts (key, get_translated_message("decrypt", message, key))
+            end
+        end
     end
 
     def get_mode()
@@ -23,7 +31,7 @@ class Cypher:
     end
     
     def get_message()
-        puts "Enter your message:"
+        puts "Enter your message"
         return gets.chomp
     end
 
@@ -38,11 +46,34 @@ class Cypher:
         # Gets the number of characters the message with get shifted by
     end
 
-    def get_translated_message()
-
+    def get_translated_message(mode, message, key)
+        if mode[0] == "d"
+            key = -key
+        end
+        translated = ""
+    
+        for symbol in 0..message.length
+            symbol_index = symbols.find(symbol)
+            if symbol_index == -1
+                # Symbol not found in SYMBOLS.
+                # Just add this symbol without any change.
+                translated += symbol
+            else
+                # Encrypt or decrypt.
+                symbol_index += key
+    
+                if symbol_index >= symbols.length
+                    symbol_index -= symbols.length
+                elsif symbol_index < 0
+                    symbol_index += symbols.length
+                end
+                translated += symbols[symbol_index]
+            end
+        return translated
     end
 end
 
 
 msg = Cypher.new
 msg.main
+    
